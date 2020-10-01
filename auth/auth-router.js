70454ece62d7390b8a1ebebf2ff1a7c09733afb9
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-30 20:43:15 -0500

    added the hash by using bcryptjs and implemented on user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const users = require("../users/users-model.js");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res, next) => {
     let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
 
     try {
         const saved = await users.add(user);
