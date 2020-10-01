51b531911f03cf43bb8e4ede3c69b51526d7cf78
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-30 20:27:33 -0500

    added basic server configuration, routes, users model, users route, auth route, migrations, seeds

---
+++ auth/auth-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const users = require("../users/users-model.js");
+
+router.post("/register", async (req, res, next) => {
+    let user = req.body;
+
+    try {
+        const saved = await users.add(user);
+        res.status(201).json(saved);
+    } catch (err) {
+        next({ apiCode: 500, apiMessage: "error registering", ...err });
+    }
+});
+
+module.exports = router;
